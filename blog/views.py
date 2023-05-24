7ff7269bc73aa4db14da4e06d824942fca1a1fb1
alotab <alotab@yahoo.com>
2023-05-24 16:39:13 +0000

    add user app

--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 
 
-posts = [
-    {
-        'title': "Blog 1",
-        'author': "Ben cafer",
-        'date_posted': " 2021-09-06",
-        'content': "i love blog",
-    }
-]
+# posts = [
+#     {
+#         'title': "Blog 1",
+#         'author': "Ben cafer",
+#         'date_posted': " 2021-09-06",
+#         'content': "i love blog",
+#     }
+# ]
 
 
 def home(request):
+    posts = Post.objects.all()
     return render(request, 'blog/home.html', context={'posts': posts})
 
 
