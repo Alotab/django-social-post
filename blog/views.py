2afa066ac1522c956bf2c7b88e28bdda374a62d9
alotab <alotab@yahoo.com>
2023-05-24 23:40:05 +0000

    add pagination

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.views.generic import (
     ListView,
@@ -16,11 +17,26 @@ def home(request):
     return render(request, 'blog/home.html', context={'posts': posts})
 
 
+
 class PostListView(ListView):
     model = Post
     template_name = 'blog/home.html'  # <app>/<model>_<viewtype>.html
     context_object_name = 'posts'
     ordering = ['-date_posted']
+    paginate_by = 2
+
+
+# post list for a specific user
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
+
 
 
 class PostDetailView(DetailView):
@@ -33,7 +49,7 @@ class PostCreateView(LoginRequiredMixin, CreateView):
     # success_url = 'blog-home'   # another way to redirect but in this case, its not working for now
     # success_url = '/' # will work i gues
 
-    
+
     # override this form_valid method and set the author to the current login user
     def form_valid(self, form):
         form.instance.author = self.request.user
